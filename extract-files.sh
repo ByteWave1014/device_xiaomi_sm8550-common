1a0b19fe6c659483869aeae516dbc62b622512fa
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-10-23 16:05:00 +0200

    sm8550-common: Allow gettid syscall for qwesd@2.0
    10-23 15:51:48.222  9383  9383 W qwesd   : libminijail[9383]: failed to get path of fd 3: No such file or directory
    10-23 15:51:48.222  9383  9383 W qwesd   : libminijail[9383]: allowing syscall: connect
    10-23 15:51:48.222  9383  9383 W qwesd   : libminijail[9383]: allowing syscall: fcntl
    10-23 15:51:48.222  9383  9383 W qwesd   : libminijail[9383]: allowing syscall: sendto
    10-23 15:51:48.222  9383  9383 W qwesd   : libminijail[9383]: allowing syscall: socket
    10-23 15:51:48.222  9383  9383 W qwesd   : libminijail[9383]: allowing syscall: writev
    10-23 15:51:48.222  9383  9383 W qwesd   : libminijail[9383]: logging seccomp filter failures
    10-23 15:51:48.223  9383  9384 E qwesd   : libminijail[9383]: blocked syscall: gettid
    
    Change-Id: I73a0b7e22e83ab477ba094dc5c2444d1b46a3cd3

--- extract-files.sh
+++ extract-files.sh
@@ -91,7 +91,8 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "vendor.libdpmframework.so" "${2}"
             "${PATCHELF}" --add-needed "libshim.so" "${2}"
-        vendor/etc/seccomp_policy/atfwd@2.0.policy | vendor/etc/seccomp_policy/modemManager.policy | vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
+        vendor/etc/seccomp_policy/atfwd@2.0.policy | vendor/etc/seccomp_policy/modemManager.policy | vendor/etc/seccomp_policy/qwesd@2.0.policy | vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
+
             [ -n "$(tail -c 1 "${2}")" ] && echo >> "${2}"
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
